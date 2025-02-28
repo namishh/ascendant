d2b977c1ac2fd391942e0553cd54dc7b8d06f238
namish <namish@tuta.io>
2025-02-28 15:33:44 +0530

    fuck networking

--- src/network.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const rl = @import("raylib");
-const PlayingCard = @import("playingcard.zig").PlayingCard;
-const Hand = @import("hand.zig").Hand;
-const Deck = @import("deck.zig").Deck;
-const PowerCards = @import("powercards.zig").PowerCards;
-const CardOverlay = @import("cardoverlay.zig").CardOverlay;
-
-// TODO: UDP networking
